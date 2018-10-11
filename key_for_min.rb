a62c4a5461c858ad48d60d0d57cd34d79b2d5119
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-11 22:23:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest = 0
+  lowest_key = nil
   name_hash.each do | key, value |
     if value < lowest
       lowest = value
+      lowest_key = key
+    end
+  return lowest_key
 end
